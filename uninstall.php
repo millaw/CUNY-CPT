e0d2113499b14962b48211c3396af8bdf981f793
Milla <milla.wynn@yahoo.com>
2025-06-17 11:26:08 -0400

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,6 @@
 <?php
 // Clean up plugin options and CPTs on uninstall
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit();
-}
+if (!defined('WP_UNINSTALL_PLUGIN')) { exit(); }
 
 // Remove dynamic CPTs and Office taxonomy options
 delete_option('cuny_dynamic_cpts');
